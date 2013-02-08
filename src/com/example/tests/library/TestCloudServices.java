072ac2fa3c83f1acc4e787df59c5b4ead487270b
oskey <dsgweb@gmail.com>
2013-02-08 09:28:51 -0200

    test

---
+++ src/com/example/tests/library/TestCloudServices.java
@@ -0,0 +1,32 @@
+package com.example.tests.library;
+
+/*
+import org.testng.Assert;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.template.pageobjects.CloudServicesPage;
+import com.template.pageobjects.HomePage;
+import com.template.util.ParentTestCase;
+*/
+/***
+ * *
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+/**
+ * 
+ * @author new
+ *
+ */
+public class TestCloudServices  {
+     //test
+	//private HomePage homePage;
+	
+}
